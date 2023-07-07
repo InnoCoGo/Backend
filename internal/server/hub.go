c6a53badd82389a0a9486956aa6d37bc3f3ff076
Ikram Kamat <ikram.kamat.04@gmail.com>
2023-07-08 02:18:47 +0300

    hub has been done

--- internal/server/hub.go
+++ internal/server/hub.go
@@ -12,3 +12,48 @@ type Hub struct {
 	Unregister chan *Client
 	Broadcast  chan *Message
 }
+
+func NewHub() *Hub {
+	return &Hub{
+		Rooms:      make(map[string]*Room),
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Broadcast:  make(chan *Message, 5),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case cl := <-h.Register:
+			if _, ok := h.Rooms[cl.RoomId]; ok {
+				r := h.Rooms[cl.RoomId]
+				// if _, ok := r.Clients[cl.Id]; !ok {
+				r.Clients[cl.Id] = cl
+				// }
+			}
+
+		case cl := <-h.Unregister:
+			if _, ok := h.Rooms[cl.RoomId]; ok {
+				clients := h.Rooms[cl.RoomId].Clients
+				if _, ok := clients[cl.Id]; ok {
+					if len(clients) != 0 {
+						h.Broadcast <- &Message{
+							Content:  "user left the chat",
+							RoomId:   cl.RoomId,
+							Username: cl.Username,
+						}
+					}
+					delete(h.Rooms[cl.RoomId].Clients, cl.Id)
+					close(cl.Message)
+				}
+			}
+		case m := <-h.Broadcast:
+			if _, ok := h.Rooms[m.RoomId]; ok {
+				for _, cl := range h.Rooms[m.RoomId].Clients {
+					cl.Message <- m
+				}
+			}
+		}
+	}
+}
